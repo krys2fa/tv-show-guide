ad616b8d4fdcecac5cc6ee788e59e5483b0e98e1
Christopher Amanor <kriskit7@gmail.com>
2020-10-13 12:23:25 +0000

    Add Notifications component

--- src/components/MainPage/mainpage.js
+++ src/components/MainPage/mainpage.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import Notifications from './Notifications/Notifications';
 
 const MainPage = () => (
-  <div className="mainpage" />
+  <div className="mainpage container">
+    <div className="row">
+      <div className="col s12 m6"></div>
+      <div className="col s12 m5 offset-m1"><Notifications /></div>
+    </div>
+  </div>
 
 );
